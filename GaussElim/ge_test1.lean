0262a452defe44d29bc73f481f5536ac996a71b3
Vivek Rajesh Joshi <vivekjoshi306@yahoo.com>
2024-05-20 23:29:53 +0530

    Update ge_test1.lean

--- GaussElim/ge_test1.lean
+++ GaussElim/ge_test1.lean
@@ -3,112 +3,6 @@ import Mathlib.Data.Matrix.Basis
 import Mathlib.Tactic.Linarith
 import GaussElim.ReducedEchelon1
 
--- variable (M : Matrix (Fin m) (Fin n) Rat) (hm : m>0) (hn : n>0)
-
--- abbrev rowListofMat := List.map List.ofFn (List.ofFn M)
-
--- lemma rowListLength_eq_numRow : (rowListofMat M).length = m := by simp
-
--- lemma rowListofMat_elt_length_eq_numCol : ∀ i, ((rowListofMat M).get i).length = n := by simp
-
--- lemma rowLengthEq : ∀ i j, (List.ofFn (M i)).length = (List.ofFn (M j)).length := by simp
-
--- abbrev colListofMat := List.map List.ofFn (List.ofFn M.transpose)
-
--- lemma colListLength_eq_numCol : (colListofMat M).length = n := by simp
-
--- /-Row-reduced form
--- 1. The first nonzero entry of every row must be 1
--- 2. Each column containing the first nonzero entry of a row must have all its other
---    entries as 0
--- -/
-
--- def row_allZerosBeforeFirstOne (row : List Rat) : Bool :=
---   List.isPrefixOf (List.replicate (row.indexOf 1) 0) row
-
--- abbrev list_allZero (l : List Rat) : Bool := l.all (fun x => (x==0))
-
--- def isRowReduced_row (ri : List Rat) : Bool×(Option Nat) :=
---   if list_allZero ri then (true,none)
---   else
---     if ri.indexOf 1 = ri.length then (false,none)
---     else
---       if row_allZerosBeforeFirstOne ri then (true,ri.indexOf 1)
---       else (false,none)
-
--- def isRowReduced_col (cj : List Rat) : Bool := List.all (List.erase cj 1) (fun x => x==0)
-
--- --In a matrix that is in row-reduced form, the index of 1 in a row that isn't all zero is less than the length of the row
--- lemma indFirstOne_lt_rowLength (rl : List Rat) (h : (isRowReduced_row rl).1 = true) (h' : ¬(list_allZero rl)) :
---   (((isRowReduced_row rl).2).getD 0) < rl.length := by
---   unfold isRowReduced_row
---   split_ifs with h1 h2
---   · have : (isRowReduced_row rl).1 == false := by unfold isRowReduced_row; rw [if_pos h1, if_neg h']; rfl
---     simp at h this; rw [h] at this; contradiction
---   · show rl.indexOf 1 < rl.length
---     apply List.indexOf_lt_length.mpr
---     rcases lt_or_gt_of_ne h1 with indlt|indgt
---     exact List.indexOf_lt_length.mp indlt
---     have l1 := rl.indexOf_le_length (a:=1)
---     have nl1 := not_le_of_gt indgt
---     contradiction
---   · have : (isRowReduced_row rl).1 == false := by
---       unfold isRowReduced_row; rw [if_neg h', if_neg h2, if_neg h1]; rfl
---     simp at h this; rw [h] at this; contradiction
-
--- def row_allZero (row : List Rat) : Bool := row.all (fun x => (x==0))
-
--- def isRowReducedAux (rl : List (List Rat)) (cl : List (List Rat)) (h : ∀ i, (rl.get i).length = cl.length) : Bool :=
---   match rl with
---   | [] => true
---   | a::as =>
---     have h0 : ∀ i, as.get i ∈ as := by intro i; rw [as.mem_iff_get]; use i
---     have h0' : ∀ i, (as.get i).length = ((a::as).get i.castSucc).length := by
---       intro i
---       obtain ⟨n,hn⟩ := ((a::as).mem_iff_get).mp ((List.subset_cons a as) (h0 i))
---       have l1 := h (Fin.castSucc i); have l2 := h n; rw [←l1, hn] at l2; exact l2
---     if h1 : row_allZero a then isRowReducedAux as cl (by intro i; have := h (i.castSucc); rw [← (h0' i)] at this; exact this)
---     else
---       if h2 : (isRowReduced_row a).1 then
---        (isRowReduced_col (cl.get ⟨(((isRowReduced_row a).2).getD 0),((h ⟨0,Nat.zero_lt_succ as.length⟩) ▸ indFirstOne_lt_rowLength a h2 h1)⟩)) ∨
---           isRowReducedAux as cl (by intro i; have := h (i.castSucc); rw [← (h0' i)] at this; exact this)
---       else false
-
--- --Checks whether matrix is in row-reduced form
--- def isRowReduced : Bool :=
---   isRowReducedAux (rowListofMat M) (colListofMat M) (by rw [colListLength_eq_numCol]; exact (rowListofMat_elt_length_eq_numCol M))
-
--- /-
--- Row-reduced echelon form
--- 1. The matrix must be row-reduced
--- 2. All rows that have only 0s must occur before those that have a nonzero entry
--- 3. If rows 1,...,r are the nonzero rows and the first nonzero entry for each of
--- these rows occurs in columns k₁,k₂,...,k_r, then  k₁< k₂<...< k_r
--- -/
-
--- --Gives list containing k₁,k₂,...,k_r
--- def nonzColIndices : List (List Rat) → List ℕ
---   | [] => []
---   | a::as =>
---       if ¬(isRowReduced_row a).1 then []
---       else [((isRowReduced_row a).2).getD 0] ++ (nonzColIndices as)
-
--- def isZeroMatrix : Bool := (rowListofMat M).all (fun x => (x.all (fun y => y==0 )))
-
--- def zeroRowsLast : Bool :=
---   let rl := rowListofMat M
---   let indOfLastNonzeroRow := rl.length-1-((rl.reverse).findIdx (fun x => (x.any (fun y => ¬(y==0)))))
---   let indsOfZeroRows := (List.unzip (rl.indexesValues (fun x => x.all (fun x => x==0)))).1
---   ([indOfLastNonzeroRow]++indsOfZeroRows).Sorted (·≤·)
-
--- def isRowReducedEchelon : Bool :=
---   (isZeroMatrix M) ∨
---     (isRowReduced M) ∧
---       (zeroRowsLast M) ∧
---         (nonzColIndices (List.filter (fun x => x.any (fun x => ¬x==0)) (rowListofMat M))).Sorted (·<·)
-
---------------------------------------------------------------------------------------------------------------------
-
 variable (M : Matrix (Fin m) (Fin n) Rat) (hm : m > 0) (hn : n > 0)
 
 def I : Matrix (Fin m) (Fin m) Rat := Matrix.diagonal (fun _ => (1:Rat))
@@ -143,10 +37,6 @@ lemma findIdx_notExists_of_eq_length (l : List α) (p : α → Bool) (hl : l.fin
   simpa using h
 
 lemma findIdx_eq_length_of_notExists (l : List α) (p : α → Bool) (hl : ∀ x∈l, p x = false) : l.findIdx p = l.length := by
-  -- contrapose hl
-  -- push_neg at *
-  -- rcases lt_or_gt_of_ne hl with lt|gt
-  -- simp; exact findIdx_exists_of_lt_length (fun x ↦ p x) l lt
   induction' l with a as ih
   · simp
   · simp at hl; have ih' := ih hl.2
@@ -259,7 +149,67 @@ def findPivot : (Option Rat)×(Fin m)×(Fin n) :=
       have nh4 := not_le.mpr (lt_of_le_of_ne (not_lt.mp h2) (Ne.symm h1))
       absurd h4 nh4
 
+abbrev rowsAfteri (i : Fin m) : Fin (m-i) → Fin m :=
+  fun k => ⟨k+i,Nat.add_lt_of_lt_sub k.2⟩
+
+abbrev colsAfterj (j : Fin n) : Fin (n-j) → Fin n :=
+  fun l => ⟨l+j,Nat.add_lt_of_lt_sub l.2⟩
+
+def botRightij (i : Fin m) (j : Fin n) : Matrix (Fin (m-i)) (Fin (n-j)) Rat :=
+  M.submatrix (rowsAfteri i) (colsAfterj j)
+
+#eval botRightij !![1,2,3;4,5,6;7,8,9] 1 1
+#eval botRightij !![1,2,3,6;2,-3,2,14;3,1,-1,2] 2 3
+#check List.get
+
+def listFind (l : List α) (p : α → Bool) (h : ∃ x ∈ l, p x) : α×(Fin (l.length)) :=
+  (l.get ⟨l.findIdx p,List.findIdx_lt_length_of_exists h⟩,⟨l.findIdx p,List.findIdx_lt_length_of_exists h⟩)
+
+#eval listFind [0,1,2,3] (fun x => x≥2) (by use 3;simp)
+#check Prod.Lex
+#check List.pmap
+
+def findPivot_ij (i : Fin m) (j : Fin n) : (Option Rat)×(Fin m)×(Fin n) :=
+  match h : (i.val,j.val) with
+  | (0,0) => (M ⟨0,hm⟩ ⟨0,hn⟩,⟨0,hm⟩,⟨0,hn⟩)
+  | (0,l+1) =>
+      have hl : (l+1) < n := by simp at h; rw [← h.2]; exact j.2
+      if h1 : list_allZero (List.ofFn (M · ⟨l+1,hl⟩)) then findPivot_ij ⟨0,hm⟩ ⟨(l+2)%n,Nat.mod_lt (l+2) hn⟩
+      else have h2 := nonzListHasNonz h1
+        ((listFind (List.ofFn (M · ⟨l+1,hl⟩)) (fun x => x≠0) (by convert (nonzListHasNonz h1);simp)).1,⟨0,hm⟩,⟨l+1,hl⟩)
+  | (k+1,0) =>
+      have hk : (k+1) < m := by simp at h; rw [← h.1]; exact i.2
+      if h1 : list_allZero (List.ofFn (M · ⟨0,hn⟩)) then findPivot_ij ⟨1%m,Nat.mod_lt 1 hm⟩ ⟨0,hn⟩
+      else have h2 := nonzListHasNonz h1
+        ((listFind (List.ofFn (M · ⟨0,hn⟩)) (fun x => x≠0) (by convert (nonzListHasNonz h1);simp)).1,⟨k+1,hk⟩,⟨0,hn⟩)
+  | (k+1,l+1) =>
+      have hl : (l+1) < n := by simp at h; rw [← h.2]; exact j.2
+      have hk : (k+1) < m := by simp at h; rw [← h.1]; exact i.2
+      if h1 : list_allZero (List.ofFn (M · ⟨l+1,hl⟩)) then findPivot_ij ⟨0,hm⟩ ⟨(l+2)%n,Nat.mod_lt (l+2) hn⟩
+      else have h2 := nonzListHasNonz h1
+        ((listFind (List.ofFn (M · ⟨l+1,hl⟩)) (fun x => x≠0) (by convert (nonzListHasNonz h1);simp)).1,⟨k+1,hk⟩,⟨l+1,hl⟩)
+  termination_by (i.val, j.val) --Lexicographic order isn't working, have to show termination by another manner
+  decreasing_by
+  · simp_wf
+    simp at h; rw [h.1]
+    right
+    -- apply Nat.sub_lt_sub_left j.2
+    sorry
+  · simp_wf
+    left
+    simp at h
+    sorry
+  · simp_wf
+    simp at h; rw [h.1]
+    left; linarith
+
+#eval findPivot_ij !![1,2,3;4,5,6;7,8,9] (by linarith) (by linarith) 0 0
+
 def pivotRowSwap : Matrix (Fin m) (Fin m) Rat :=
+  if (findPivot M hm hn).1 = none then 1
+  else eltRowSwap ⟨0,hm⟩ ((findPivot M hm hn).2).1
+
+def pivotRowSwap_ij (i : Fin m) (j : Fin n) : Matrix (Fin m) (Fin m) Rat :=
   if (findPivot M hm hn).1 = none then 1
   else eltRowSwap ⟨0,hm⟩ ((findPivot M hm hn).2).1
 
@@ -290,3 +240,12 @@ def e2 := pivotImprove exmat (by linarith) (by linarith)
 def elist := pivotColElimBelow (e2 * e1 * exmat) (by linarith) (by linarith)
 #eval elist
 #eval (List.prod (elist.reverse++[e2,e1]))*exmat
+
+#check Matrix.toBlock
+
+def bot2x2 : Fin 2 → Fin 3
+| 0 => 1
+| 1 => 2
+
+
+#eval !![1,2,3;4,5,6;7,8,9].submatrix bot2x2 bot2x2
